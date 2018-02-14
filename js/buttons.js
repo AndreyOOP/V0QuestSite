e4feb771c631f690a899e678a977bb6c91537b85
AndreyOOP <andrew_yudaichev@i.ua>
2018-02-14 21:28:12 +0200

    Action Buttons Update
    - add new buttons default and hover pictures
    - update button.js to maintain it
    - update index.html to display these buttons

--- js/buttons.js
+++ js/buttons.js
@@ -2,10 +2,19 @@ function hoverf(x){
 
     if(x.src.includes("book.png"))
         x.src = 'pictures/book-hover.png';
+    else if(x.src.includes("map.png"))
+        x.src = 'pictures/map-hover.png';
+    else if(x.src.includes("summary.png"))
+        x.src = 'pictures/summary-hover.png';
+
 }
 
 function leavef(x){
 
     if(x.src.includes("book-hover.png"))
         x.src = 'pictures/book.png';
+    else if(x.src.includes("map-hover.png"))
+        x.src = 'pictures/map.png';
+    else if(x.src.includes("summary-hover.png"))
+        x.src = 'pictures/summary.png';
 }
